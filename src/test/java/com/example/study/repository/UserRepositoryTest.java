40451adc5cd3dd993e8f994d844b69c0804ac9e1
hokim <teamgoodaloe@gmail.com>
2021-03-25 22:45:44 +0900

    10. JPA를 통한 CRUD 배우기 - 1_2

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -5,7 +5,9 @@ import com.example.study.model.entity.User;
 import com.example.study.model.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
     //dependency injection (DI)
@@ -22,8 +24,26 @@ public class UserRepositoryTest extends StudyApplicationTests {
         user.setCreatedBy("admin");
         User saved = userRepository.save(user);
         System.out.println(saved);
-        //showAll();
     }
 
+    @Test
+    public void read() {
+        Optional<User> user = userRepository.findById(1L);
+        user.ifPresent(selecteduser->
+        System.out.println(selecteduser)
+        );
+
+    }
+
+    @Test
+    public void update() {
+        Optional<User> user = userRepository.findById(1L);
+        user.ifPresent(selectedUser->
+        {
+            selectedUser.setAccount("pppp");
+            userRepository.save(selectedUser);
+        }
+        );
+    }
 
 }
